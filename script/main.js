12dc9e3a722106fe47a1e1a05910ff2880a7de88
FlyvioJR <flaviotaylorr@gmail.com>
2020-06-22 19:41:41 -0300

    bootStrap adicionado e validacoes de pesquisa

--- script/main.js
+++ script/main.js
@@ -1,17 +1,47 @@
-function consultaCep(){
-
-    var cep = document.getElementById("cep").value;
-    var url = "https://viacep.com.br/ws/" + cep + "/json/";
-
-    $.ajax({
-        url: url,
-        type: "GET",
-        success: function(response){
-            console.log(response);
-        }
+function validaCep(cep){
+    if (cep != "" && cep.replace("-","").length === 8) {
+        return true;
+    } else {
+        return false
     }
-    );
+}
+
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+
+    if (validaCep(cep)){
+        $(".progress").show();
+        $(".cep-table").hide();
+        
+        setTimeout(() => {  
+            var url = "https://viacep.com.br/ws/" + cep + "/json/";
+            $.ajax({
+                url: url,
+                type: "GET",
+                success: function(response){
+                    if (response.erro){
+                        alert("cep inválido!")
+                        $(".progress").hide();
+                        $(".cep-table").hide();
+                    } else {
+                        $("#cep-pesquisado").html("CEP: " + response.cep);
+                        $("#logradouro").html(response.logradouro);
+                        $("#bairro").html(response.bairro);
+                        $("#localidade").html(response.localidade);
+                        $("#uf").html(response.uf);
+                        $(".cep-table").show();
+                        $(".progress").hide();}
+                }
+            });
+        }, 500);
+    } else{
+        $(".progress").hide();
+        $(".cep-table").hide();
+        alert("Digite um cep válido!");
+    }
 };
 
-
-
+$(function(){
+    $(".cep-table").hide();
+    $(".progress").hide();
+});
